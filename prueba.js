ac58aedf6c5bb2d9a4e7c6308eaba048f241a563
Francisco Pane <qwerfaqs@gmail.com>
2016-02-27 02:35:09 -0300

    archivos iniciales
    archivos

---
+++ prueba.js
@@ -0,0 +1,6 @@
+function hola(nombre) {
+    return "Hola, " + nombre;
+}
+//var nombre = "Franco";
+var nombre = ["Franco", "Daniel", "Pane"];
+document.body.innerHTML = hola(nombre);
---
+++ prueba.ts
@@ -0,0 +1,11 @@
+
+
+
+function hola(nombre: string) {
+    return "Hola, "+nombre;
+}
+
+
+var nombre = "Franco";
+//var nombre = ["Franco", "Daniel", "Pane"];
+document.body.innerHTML = hola(nombre);
